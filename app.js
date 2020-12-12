8cca6bba060f5489abdaf0dbcec253aa79819b4c
Abhinav Jha <Abhi0803@github.com>
2020-12-12 14:58:39 +0530

    Added a Procfile.

--- app.js
+++ app.js
@@ -1,79 +1,39 @@
-import express, { static } from "express";
-import { urlencoded } from "body-parser";
-import ejs from "ejs";
-import { capitalize, truncate } from "lodash";
-import { connect, model } from "mongoose";
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const homeStartingContent =
-  "This is a simple Blog website connected with MongoDB and if you want to compose a post jusr type a new name after the url /new_post_name";
+  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
-  "wlijdalskdiakakjls dajdla sdjaskd aljdaj podwjp jdpaodjpoawjdas  aasd ada awjodijladklajsdad jdpaodjpoawjdasa dpoawujdauwodiaw a dadaswdasdasdasdsafsg fasfasdf sfs s fs df df sfasd fsfsadfdas sa";
+  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
-  "wlijdalskdiakakjls dajdla sdjaskd aljdaj podwjp jdpaodjpoawjdas  aasd ada awjodijladklajsdad jdpaodjpoawjdasa dpoawujdauwodiaw a dadaswdasdasdasdsafsg fasfasdf sfs s fs df df sfasd fsfsadfdas sa";
+  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 
 app.set("view engine", "ejs");
 
-app.use(
-  urlencoded({
-    extended: true,
-  })
-);
-app.use(static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-mongoose.connect(
-  "mongodb+srv://Red:8268@cluster0.sq4z9.mongodb.net/blogdb?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-  }
-);
+mongoose.connect("mongodb://localhost:27017/blogDB", { useNewUrlParser: true });
 
-const documentSchema = {
+const postSchema = {
   title: String,
-  compose: String,
-  truncated: String,
+  content: String,
 };
 
-const Document = model("Document", documentSchema);
-
-const post1 = new Document({
-  title: "Day 1",
-  compose: "Hello How are You??",
-  truncated: "Hello How are You??",
-});
-
-const post2 = new Document({
-  title: "Day 2",
-  compose: "Hello are You??",
-  truncated: "Hello How are You??",
-});
+const Post = mongoose.model("Post", postSchema);
 
 app.get("/", function (req, res) {
-  Document.find({}, function (err, results) {
-    if (results.length === 0) {
-      post1.save();
-      post2.save();
-      res.redirect("/");
-    } else {
-      res.render("home", {
-        homeStartingContent: homeStartingContent,
-        posts: results,
-        truncated: results,
-      });
-    }
-  });
-});
-
-app.get("/about", function (req, res) {
-  res.render("about", {
-    aboutContent: aboutContent,
-  });
-});
-
-app.get("/contact", function (req, res) {
-  res.render("contact", {
-    contactContent: contactContent,
+  Post.find({}, function (err, posts) {
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts,
+    });
   });
 });
 
@@ -81,61 +41,40 @@ app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
-app.get("/delete", function (req, res) {
-  Document.find({}, function (err, results) {
-    res.render("delete", {
-      homeStartingContent: homeStartingContent,
-      posts: results,
-      truncated: results,
-    });
+app.post("/compose", function (req, res) {
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody,
   });
-});
 
-app.post("/delete", function (req, res) {
-  const titles = req.body.delete;
-
-  Document.findOneAndRemove({ title: titles }, function (err, results) {
+  post.save(function (err) {
     if (!err) {
       res.redirect("/");
     }
   });
 });
 
-app.get("/posts/:post", function (req, res) {
-  const requestedTitle = capitalize(req.params.post);
-  Document.findOne(
-    {
-      title: requestedTitle,
-    },
-    function (err, results) {
-      if (!err) {
-        if (!results) {
-          res.render("error");
-        } else {
-          res.render("post", {
-            title: results.title,
-            content: results.compose,
-          });
-        }
-      }
-    }
-  );
-});
+app.get("/posts/:postId", function (req, res) {
+  const requestedPostId = req.params.postId;
 
-app.post("/compose", function (req, res) {
-  const title = capitalize(req.body.title);
-  const compose = req.body.compose;
-  const save = new Document({
-    title: title,
-    compose: compose,
-    truncated: truncate(compose, {
-      length: 150,
-    }),
+  Post.findOne({ _id: requestedPostId }, function (err, post) {
+    res.render("post", {
+      title: post.title,
+      content: post.content,
+    });
   });
-  save.save();
-  res.redirect("/");
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log("server started at 3000");
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
 });
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
+});
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
